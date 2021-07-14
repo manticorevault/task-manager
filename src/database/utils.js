45e0f1591a8c816bf7f3a87175f28d150f34f177
manticorevault <artur_seilah@hotmail.com>
2021-07-14 16:04:52 -0300

    Created utils.js to load SQL data queries

--- src/database/utils.js
+++ src/database/utils.js
@@ -1,2 +1,21 @@
 const fs = require("fs-extra");
 const { join } = require("path");
+
+const loadQueries = async dirName => {
+    const filePath = join(process.cwd(), "src", "data", dirName);
+
+    const files = await fs.readdir(filePath)
+
+    const dataFiles = files.filter( f => f.endsWith(".sql"))
+    const dataQueries = {};
+
+    for (const dataFile of dataFiles) {
+        const singleQuery = fs.readFileSync(join(filePath, dataFile), { encoding: "UTF-8" });
+        
+        dataQueries[dataFile.replace(".sql", "")] = singleQuery;
+    }
+
+    return dataQueries;
+}
+
+module.exports = { loadQueries };
