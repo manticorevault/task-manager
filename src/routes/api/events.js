eb75108f0eeba392dfb8e0db84bfccfc9c620396
manticorevault <artur_seilah@hotmail.com>
2021-07-14 18:45:50 -0300

    Updated events

--- src/routes/api/events.js
+++ src/routes/api/events.js
@@ -5,14 +5,14 @@ module.exports.register = async server => {
         handler: 
             async request => {
                 try {
-                    const db = request.server.plugins.sql.client
+                    const db = request.server.plugins.sql.client;
 
                     // Hardcoded userId for tests
                     const userId ="testUser";
                     const res = await db.events.getEvents(userId);
 
                     return res.recordset;
-                    
+
                 } catch (err) {
                     console.log(err)
                 }
