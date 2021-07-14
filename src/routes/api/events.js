5b0e4fd42c550dfabe423cbcc6aa13f4e51f7d04
manticorevault <artur_seilah@hotmail.com>
2021-07-14 17:09:54 -0300

    API Handler for events

---
+++ src/routes/api/events.js
@@ -0,0 +1,21 @@
+module.exports.register = async server => {
+    server.route({
+        method: "GET",
+        path: "/api/events",
+        handler: 
+            async request => {
+                try {
+                    const db = request.server.plugins.sql.client
+
+                    // Hardcoded userId for tests
+                    const userId ="testUser";
+                    const res = await db.events.getEvents(userId);
+
+                    return res.recordset;
+                    
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+    })
+}
