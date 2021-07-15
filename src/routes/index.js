4400d79ccb271d2dcc35fd5c33515d2b5b32f1e2
manticorevault <artur_seilah@hotmail.com>
2021-07-15 15:21:01 -0300

    Updated the front page to handle the views created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,32 @@
 const api = require("./api");
+const auth = require("./auth");
 
 module.exports.register = async server => {
 
     await api.register(server);
+    await api.register(auth);
 
     server.route({
         method: "GET",
         path:"/",
         handler: async(request, h) => {
-            return "It's working!"
+            return h.view("index", { title: "Home" });
+        },
+        options: {
+            auth: {
+                mode: "try"
+            }
         }
     });
-}
+
+    server.route({
+        method: "GET",
+        // Provide a catchAll route
+        path: "/{param*}",
+        handler: {
+            directory: {
+                path: "dist"
+            }
+        }
+    });
+};
