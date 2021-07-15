d6bc9d6e15809805ec694a59bb9740727f720c19
manticorevault <artur_seilah@hotmail.com>
2021-07-15 16:21:41 -0300

    Updated events.js to handle task creation, update and deletion

--- src/routes/api/events.js
+++ src/routes/api/events.js
@@ -5,7 +5,7 @@ module.exports.register = async server => {
         method: "GET",
         path: "/api/events",
         options: {
-            auth: { mode: try }
+            auth: { mode: "try" }
         },
         handler: 
             async request => {
@@ -26,4 +26,103 @@ module.exports.register = async server => {
                 }
             }
     })
+
+    server.route({
+        method: "POST",
+        path: "api/events",
+        options: {
+            auth: { mode: "try" }
+        },
+        handler:
+            async request => {
+                try {
+                    if (!request.auth.isAuthenticated) {
+                        return boom.unauthorized();
+                    }
+
+                    const db = request.server.plugins.sql.client;
+                    const userId = request.auth.credentials.profile.id;
+                    const {
+                        startDate,
+                        startTime,
+                        endDate,
+                        endTime,
+                        title,
+                        description
+                    } = request.payload;
+                    const res = await db.events.addEvent({ userId, startDate, startTime, endDate, endTime, title, description });
+                    return res.recordset[0];
+
+                } catch (err) {
+                    console.log(err);
+
+                    return boom.boomify(err);
+                }
+            }
+    });
+
+    server.route({
+        method: "PUT",
+        path: "api/events/{id}",
+        options: {
+            auth: { mode: "try" }
+        },
+        handler:
+            async request => {
+                try {
+                    if (!request.auth.isAuthenticated) {
+                        return boom.unauthorized();
+                    }
+
+                    const db = request.server.plugins.sql.client;
+                    const userId = request.auth.credentials.profile.id;
+                    const {
+                        startDate,
+                        startTime,
+                        endDate,
+                        endTime,
+                        title,
+                        description
+                    } = request.payload;
+                    const res = await db.events.addEvent({ userId, startDate, startTime, endDate, endTime, title, description });
+                    return res.recordset[0];
+
+                } catch (err) {
+                    console.log(err);
+
+                    return boom.boomify(err);
+                }
+            }
+    });
+
+    server.route({
+        method: "DELETE",
+        path: "/api/events/{id}",
+        options: {
+            auth: { mode: "try" }
+        },
+        handler: 
+            async (request, h) => {
+               try {
+                   if (!request.auth.isAuthenticated) {
+                       return boom.unauthorized();
+                   }
+
+                   const id = request.params.id;
+                   const userId = request.auth.credentials.profile.id;
+                   const db = request.server.plugins.sql.client;
+                   const res = await db.events.deleteEvent({ id, userId });
+
+                   return res.rowsAffected[0] === 1 
+                        ? h.response().code(204)
+                        : boom.notFound();
+               } catch (err) {
+                   console.log(err);
+
+                   return boom.boomify(err);
+               }
+            }
+    })
+
+
 }
