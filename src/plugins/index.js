ac7db8cb2bfad57202b6fe54f01af4b6369fb9a2
manticorevault <artur_seilah@hotmail.com>
2021-07-15 14:29:02 -0300

    Updated the plugin's index.js to handle the new packages and views

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,19 @@
+const ejs = require("ejs");
+const inert = require("@hapi/inert");
+const vision = require("@hapi/vision");
 const sql = require("./sql");
+const auth = require("./auth");
 
 module.exports.register = async server => {
-    await server.register(sql);
+    await server.register([
+        auth,
+        inert,
+        vision,
+        sql
+    ]);
+
+    server.views({
+        engines: { ejs },
+        relativeTo: __dirname,
+    })
 }
